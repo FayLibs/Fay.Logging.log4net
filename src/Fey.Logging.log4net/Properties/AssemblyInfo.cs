c307ebf87e5d8decb2948799128881c51ade421a
Rodney S. Foley <CreepyGnome@users.noreply.github.com>
2016-01-06 10:15:46 -0700

    updated addembly info details

--- src/Fey.Logging.log4net/Properties/AssemblyInfo.cs
+++ src/Fey.Logging.log4net/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fey.Logging.log4net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Fey Logging Library for log4net")]
+[assembly: AssemblyDescription("Facade for logging to the log4net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fey.Logging.log4net")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Rodney S. Foley")]
+[assembly: AssemblyProduct("Fey Logging Library for log4net")]
+[assembly: AssemblyCopyright("Copyright © 2015 Rodney S. Foley")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
